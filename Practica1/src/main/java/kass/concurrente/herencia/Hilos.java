15f64ea984f6332d7d0c8922734bb907fa2e528d
diegodgl10 <diego.dgl10@ciencias.unam.mx>
2024-02-08 23:39:50 -0600

    Introduccion a Hilos

--- Practica1/src/main/java/kass/concurrente/herencia/Hilos.java
+++ Practica1/src/main/java/kass/concurrente/herencia/Hilos.java
@@ -1,14 +1,36 @@
 package kass.concurrente.herencia;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * En esta clase, debes crear un contador extendiendo de la clase Thread
  * @author Kassandra Mirael
  * @version 1.1
  */
 public class Hilos extends Thread {
-    public static final Integer HILOS = 3;
+    // Lista de hilos
+    static List<Thread> lHilos = new ArrayList<>();
+
+    // Metodo run de la clase Thread
+    @Override
+    public void run() {
+        System.out.println("Hola soy el: "+ Thread.currentThread().getName());
+    }
 
     public static void main(String[] args) throws InterruptedException {
-
+        int nHilos = 10;
+        Hilos h = new Hilos();
+        Thread thread;
+        for (int i = 0; i < nHilos; i++){
+            thread = new Thread(h, Integer.toString(i));
+            lHilos.add(thread);   
+        }
+        for (Thread t : lHilos) {
+            t.start();
+        }
+        for (Thread t : lHilos) {
+            t.join();
+        }
     }
 }
